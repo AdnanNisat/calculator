235e178cf902207b485629dbf05cbce79e59dba8
Adnan <adnannisat@gmail.com>
2022-10-29 00:23:18 +0600

    Display numbers when button is clicked, make clear button, unfinished evaluation code

--- script.js
+++ script.js
@@ -27,3 +27,62 @@ function operate(number1, operator, number2){
   else if( operator === '/' );
     return divide(number1,number2);
 };
+
+function display(symbol){
+  const display = document.getElementById("display");
+  display.textContent = symbol;
+};
+
+function clear(){
+  const display = document.getElementById("display");
+  display.textContent = '';
+};
+
+// Global Variables
+
+let str = '';
+let operator = '';
+let num1 = 0;
+let num2 = 0;
+let result = 0;
+let click_count = 0;
+//
+const btn = document.querySelectorAll('.num');
+
+btn.forEach((item) => {
+  item.addEventListener('click', () => {
+    let content = item.textContent;
+    str = str + content;
+    display(str);  
+  });  
+});
+
+const clear_btn = document.querySelector('#clear');
+clear_btn.addEventListener('click', () => {
+  clear();
+  str = '';
+});
+
+const operators = document.querySelectorAll('.operator');
+operators.forEach((item) => {
+  item.addEventListener('click', () => {
+    click_count++;
+    operator = item.textContent;
+
+    if(click_count % 2 === 0)
+      num1 = parseInt(str);
+    else 
+     num2 = parseInt(str);
+
+    str = '';
+    if(click_count > 1){
+      result = operate(num1,operator,num2);
+    }
+      
+  });
+});
+
+const evaluate = document.querySelector('#evaluate');
+evaluate.addEventListener('click', () => {
+  display(result);
+});
